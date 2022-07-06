eb6422ae396255df0af379e8eec1048db67ef9ad
Pedro <pedroviniciusgonzaga@hotmail.com>
2022-07-06 19:14:44 -0300

    Adicionando models de database

--- models/personagens.py
+++ models/personagens.py
@@ -1,6 +1,7 @@
+from unicodedata import name
 from db import db
 
-class BookModel(db.Model):
+class Personagens(db.Model):
     __tablename__ = 'personagem'
     
     id = db.Column(db.Integer, primary_key=True)
@@ -24,4 +25,103 @@ class BookModel(db.Model):
     insanity = db.Column(db.Integer)
     dinheiro = db.Column(db.Float)
     itens = db.Column(db.String(100))
+
+
+    def __init__(
+        self,
+        id,
+        name,
+        level,
+        hp,
+        martial,
+        handgun,
+        bow,
+        rifle,
+        smg,
+        shotgun,
+        sniper,
+        white,
+        blunt,
+        stealth,
+        carisma,
+        strenght,
+        agility,
+        dodge,
+        insanity,
+        dinheiro,
+        itens
+    )
+
+    def __repr__(self):
+        
+        return f'Personagens(
+        id={self.id},
+        name={self.name},
+        level={self.level},
+        hp={self.hp},
+        martial={self.martial},
+        handgun={self.handgun},
+        bow={self.bow},
+        rifle={self.rifle},
+        smg={self.smg},
+        shotgun={self.shotgun},
+        sniper={self.sniper},
+        white={self.white},
+        blunt={self.blunt},
+        stealth={self.stealth},
+        carisma={self.carisma},
+        strenght={self.strenght},
+        agility={self.agility},
+        dodge={self.dodge},
+        insanity={self.insanity},
+        dinheiro={self.dinheiro},
+        itens={self.itens}
+        )'
     
+
+    def json(self):
+        return {
+            id,
+        'name': self.name,
+        'level': self.level,
+        'hp': self.hp,
+        'martial': self.martial,
+        'handgun': self.handgun,
+        'bow': self.bow,
+        'rifle': self.rifle,
+        'smg': self.smg,
+        'shotgun': self.shotgun,
+        'sniper': self.sniper,
+        'white': self.white,
+        'blunt': self.blunt,
+        'stealth': self.stealth,
+        'carisma': self.carisma,
+        'strenght': self.strenght,
+        'agility': self.agility,
+        'dodge': self.dodge,
+        'insanity': self.insanity,
+        'dinheiro': self.dinheiro,
+        'itens': self.itens
+        }
+
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+        
